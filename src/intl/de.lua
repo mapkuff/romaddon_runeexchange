3dd45760409a560e602cfcc011d1bd3653f168a7
mapkuff <nongmapza>
2024-12-09 03:10:31 +0700

    add german translation

---
+++ src/intl/de.lua
@@ -0,0 +1,43 @@
+
+
+
+return {
+  RuneNames = {
+--     -- T6
+    Enigma = "Rätsel",
+    Judge = "Richter",
+    Massacre = "Massaker",
+--     -- T5
+    Tyrant = "Tyrann",
+    Assassin = "Assassine",
+    Sage = "Weiser",
+--     -- T4
+    Curse = "Fluch",
+    Raid = "Schlachtzug",
+    Accuracy = "Präzision",
+--     -- T3
+    Destruction = "Zerstörung",
+    Enchantment = "Verzauberung",
+    Dauntlessness = "Unerschrockenheit",
+    Madness = "Wahnsinn",
+--     -- T2
+    Grasp = "Auffassungsgabe",
+    Capability = "Leistungsfähigkeit",
+    Keenness = "Versessenheit",
+    Comprehension = "Begreifen",
+    Ferocity = "Bösartigkeit",
+--     -- BASE
+    WIS = "Geist",
+    STR = "Wild",
+    MP = "Zauber",
+    STA = "Ausdauer",
+    HP = "Leben",
+    AGI = "Geschicklichkeit",
+    INT = "Intellekt",
+  },
+  linkCaptions = {
+    Runes = "Runen",
+    WeaponRune = "Waffen Rune",
+    Tier = "Grad",
+  }
+};
